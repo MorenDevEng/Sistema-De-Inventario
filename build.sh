1a254f8b89454468e998ad89cd415af7ed835bdd
MorenoDev <moreno.deveng@gmail.com>
2026-01-19 12:21:17 -0400

    Fix: vercel.json

--- build.sh
+++ build.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
 echo "--- 1. Instalando dependencias de Python ---"
-python3.11 -m pip install -r requirements.txt
+# En Vercel Build, usamos python3
+python3 -m pip install -r requirements.txt
 
 echo "--- 2. Instalando dependencias de Node (Tailwind) ---"
 npm install
 
 echo "--- 3. Compilando Tailwind CSS ---"
-# Esto crea el archivo output.css basándose en el input.css
 npx tailwindcss -i ./Apps/static/css/input.css -o ./Apps/static/css/output.css --minify
 
 echo "--- 4. Recolectando archivos estáticos (Collectstatic) ---"
-# Esto toma el output.css generado y lo mete en la carpeta 'staticfiles' para WhiteNoise
-python3.11 manage.py collectstatic --noinput --clear
-
-#!/bin/bash
-# ... (todos los comandos anteriores: pip install, npm install, tailwind, collectstatic)
-
-# Aseguramos que la carpeta de salida exista para Vercel
-mkdir -p staticfiles
+# Cambiamos a python3
+python3 manage.py collectstatic --noinput --clear
 
 echo "--- ¡PROCESO FINALIZADO EXITOSAMENTE! ---"
