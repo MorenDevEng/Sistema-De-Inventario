11d502dffb48ed3d5b6a9e368fa3ac31f5fa0be6
MorenoDev <moreno.deveng@gmail.com>
2026-01-19 09:40:18 -0400

    Ajuste static y staticfiles

--- Dashboard_Productos/settings.py
+++ Dashboard_Productos/settings.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_ENV = Path(__file__).resolve().parent.parent
+
 # Cargar el archivo .env
 load_dotenv()
 # Quick-start development settings - unsuitable for production
@@ -135,11 +135,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATICDIRS = {
-   BASE_DIR / 'Apps/static',
-}
+STATICFILES_DIRS = [
+    BASE_DIR / 'Apps/static',
+    ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
