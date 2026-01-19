738fa230b20f9db203de6c5e4a992b8d7bb3b0ed
MorenoDev <moreno.deveng@gmail.com>
2026-01-19 10:30:01 -0400

    Host

--- Dashboard_Productos/settings.py
+++ Dashboard_Productos/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = [
-    '*',
+    'sistema-de-inventario-five.vercel.app',
+    '.vercel.app'
 ]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
