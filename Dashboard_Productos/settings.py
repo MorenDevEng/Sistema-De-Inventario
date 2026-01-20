c775f61f657831ac36daffa66b6e273c2aca4c7e
MorenoDev <moreno.deveng@gmail.com>
2026-01-19 20:33:58 -0400

    Ajuste del settings debugging

--- Dashboard_Productos/settings.py
+++ Dashboard_Productos/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # settings.py
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = [
     '*',
