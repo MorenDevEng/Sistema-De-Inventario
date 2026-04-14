c7b0aabf86339ce245cfd00d096ed4c1dd3df2d7
MorenoDev <moreno.deveng@gmail.com>
2026-04-14 09:18:59 -0400

    feat: agregar documentación del proyecto y mejoras visuales en templates y CSS

--- Dashboard_Productos/settings.py
+++ Dashboard_Productos/settings.py
@@ -95,17 +95,29 @@ WSGI_APPLICATION = 'Dashboard_Productos.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
+# DATABASES = {
+#     'default': {
+#         "ENGINE": 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT'),
+#         'OPTIONS': {
+#             "sslmode":"require",
+#         }
+#     }
+# }
+
+
 DATABASES = {
     'default': {
         "ENGINE": 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-        'OPTIONS': {
-            "sslmode":"require",
-        }
+        'NAME': 'Dashboard_Datos',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
